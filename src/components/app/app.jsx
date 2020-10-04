76665b6b195a680d6df3a91083f4d1749b49bbc4
Kartaviy-arteom <kartaviy_arteom@mail.ru>
2020-10-04 12:25:13 +0300

    маршрутизация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import WelcomePage from "../welcome-page/welcome-page";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import ArtistQuestion from "../artist-question/artist-question";
+import GenreScreen from "../genre-screen/genre-screen";
+import LoginScreen from "../login-screen/login-screen";
+import SuccessScreen from "../success-screen/success-screen";
+import FailScreen from "../fail-screen/fail-screen";
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomePage errorsCount = {errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path ="/dev-artist" >
+          <ArtistQuestion />
+        </Route>
+        <Route exact path ="/dev-genre" >
+          <GenreScreen />
+        </Route>
+        <Route exact path ="/login" >
+          <LoginScreen />
+        </Route>
+        <Route exact path ="/result" >
+          <SuccessScreen />
+        </Route>
+        <Route exact path ="/lose" >
+          <FailScreen />
+        </Route>
+        <Route>
+          <WelcomePage errorsCount = {errorsCount} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
