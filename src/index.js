3c2468bb6d816f5e835ae3f82614dd7b1df5fb6e
Kartaviy-arteom <kartaviy_arteom@mail.ru>
2020-10-11 17:21:02 +0300

    моки для игры

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
+import questions from "./mocks/questions";
 
 const ERROR_COUNT = 3;
 
 const rootElement = document.querySelector(`#root`);
 
 ReactDOM.render(
-    <App errorsCount = {ERROR_COUNT} />,
+    <App errorsCount = {ERROR_COUNT} questions = {questions} />,
     rootElement
 );
